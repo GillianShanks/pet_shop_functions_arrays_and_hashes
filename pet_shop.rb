a2c338bf9bf294f1d4be1dcfed6722a5558ec3c2
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-19 15:36:34 +0100

    remove_pet_by_name passed test

--- pet_shop.rb
+++ pet_shop.rb
@@ -41,3 +41,12 @@ def find_pet_by_name (pet_shop, pet_name)
   end
   return nil
 end
+
+def remove_pet_by_name (pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+     if pet[:name] == pet_name
+       pet_shop[:pets].delete(pet)
+     end
+  end
+  return nil
+end
